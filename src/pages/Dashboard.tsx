e1d152dc54637886dcd73f5544069812e1021a97
vorsakha <andreitf.dev@gmail.com>
2021-09-09 21:30:31 -0300

    Dashboard - clean

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,22 +1,9 @@
-import { Link, useLocation } from "react-router-dom";
 import DataTable from "../components/DataTable";
 import Search from "../components/Search";
-import { useAppSelector } from "../redux/hooks";
 
 const Dashboard = () => {
-  const { data } = useAppSelector((state) => state.apiReducer);
-
-  let location = useLocation();
   return (
     <div>
-      <Link
-        to={{
-          pathname: `/details/${data[0]?.login.salt}`,
-          state: { background: location },
-        }}
-      >
-        Link modal
-      </Link>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
         deleniti modi culpa ipsa. Voluptatem fuga eius quis dicta fugit itaque
