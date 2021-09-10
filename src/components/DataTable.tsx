ee23eaacf7b5ad679fd2996b182aa2554fde262e
vorsakha <andreitf.dev@gmail.com>
2021-09-09 21:32:37 -0300

    DataTable - add table structure and data

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,9 +1,47 @@
+import { Link, useLocation } from "react-router-dom";
 import { useAppSelector } from "../redux/hooks";
+import formatDate from "../utils/formatDate";
 
 const DataTable = () => {
   const { searchData } = useAppSelector((state) => state.searchReducer);
 
-  return <div>table</div>;
+  const location = useLocation();
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Gender</th>
+          <th>Birth</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {searchData?.map((item) => {
+          const name = `${item.name.first} ${item.name.last}`;
+
+          return (
+            <tr key={item.login.salt}>
+              {" "}
+              <td>{name}</td>
+              <td>{item.gender}</td>
+              <td>{formatDate(item.dob.date)}</td>
+              <td>
+                <Link
+                  to={{
+                    pathname: `/details/${item.login.salt}`,
+                    state: { background: location },
+                  }}
+                >
+                  View
+                </Link>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default DataTable;
