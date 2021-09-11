aee6901f7ed3892ec3a10f13af8e5fd9749a9096
vorsakha <andreitf.dev@gmail.com>
2021-09-10 22:05:06 -0300

    Layout - add styles / move fetch patients

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,15 +10,17 @@ const Layout: React.FC = ({ children }) => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    data.length === 0 && dispatch(getPatients());
-  });
+    if (data.length === 0 || data === null) {
+      dispatch(getPatients());
+    }
+  }, []);
 
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <Header />
 
-      <div>
-        <p>
+      <div className="max-w-3xl flex flex-col mx-auto p-4 py-14 leading-7">
+        <p className="my-4">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
           deleniti modi culpa ipsa. Voluptatem fuga eius quis dicta fugit itaque
           nobis ipsam ipsa corrupti. Illum cupiditate nulla delectus blanditiis
