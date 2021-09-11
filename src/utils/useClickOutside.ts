c865e17c50aebbeb491372c90df6675050ad6d6f
vorsakha <andreitf.dev@gmail.com>
2021-09-10 21:58:40 -0300

    utils - add click outside hook

---
+++ src/utils/useClickOutside.ts
@@ -0,0 +1,23 @@
+import { useEffect, MutableRefObject } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function useClickOutside(
+  ref: MutableRefObject<HTMLDivElement | null>,
+  action: () => void
+) {
+  const history = useHistory();
+
+  ref === undefined &&
+    console.error("ref is not reachable | useOutsideClick hook");
+
+  useEffect(() => {
+    function handleClickOutside(event: Event) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        action();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [ref, history, action]);
+}
