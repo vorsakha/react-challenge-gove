cbc811e5c8823482be2d684b7305815ca7ba9b5c
vorsakha <andreitf.dev@gmail.com>
2021-09-10 22:05:40 -0300

    Header - add styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,22 @@
-import React from "react";
 import { IoLogoBuffer as Logo } from "@react-icons/all-files/io5/IoLogoBuffer";
 import { AiOutlineUser as UserIcon } from "@react-icons/all-files/ai/AiOutlineUser";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header className="bg-white flex flex-row justify-between items-center h-16">
+      <Link
+        to="/"
+        className="flex flex-row gap-2 text-3xl items-center font-bold pl-4"
+      >
         <Logo />
         Company
-      </div>
+      </Link>
 
-      <div>
-        <UserIcon />
+      <div className="text-2xl pr-4">
+        <div className="rounded-full bg-gray-200 p-3">
+          <UserIcon />
+        </div>
       </div>
     </header>
   );
