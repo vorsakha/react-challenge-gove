03881d4e5a61754979f04c7a19712ce2cb0435e3
vorsakha <andreitf.dev@gmail.com>
2021-09-10 22:13:02 -0300

    Details - add styles / translate to english / limit items per page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,18 +1,37 @@
-import { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
+import { RiCloseLine as CloseIcon } from "@react-icons/all-files/ri/RiCloseLine";
 import { DetailedData } from "../../types";
-import { useAppSelector } from "../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import formatDate from "../utils/formatDate";
+import handleBlockScroll from "../utils/handleBlockScroll";
+import useClickOutside from "../utils/useClickOutside";
+import { setCurrentPage } from "../redux/page/slice";
 
 const Details = () => {
   const [detailed, setDetailed] = useState<DetailedData | null>(null);
 
+  const dispatch = useAppDispatch();
+
+  const { searchData } = useAppSelector((state) => state.searchReducer);
+  const { currentPage } = useAppSelector((state) => state.pageReducer);
+
+  const itemsPerPage = 9;
+
+  const history = useHistory();
+
   const { id } = useParams<{ id: string }>();
 
-  const { data } = useAppSelector((state) => state.apiReducer);
+  const ref = useRef(null);
+  const handleCloseButton = () => {
+    handleBlockScroll(false);
+
+    history.push(`/page/${currentPage}`);
+  };
+  useClickOutside(ref, handleCloseButton);
 
   const filterData = () => {
-    const newData = data.filter((item) => item.login.salt === id)[0] || null;
+    const newData = searchData[Number(id) - 1];
 
     setDetailed(newData);
   };
@@ -21,43 +40,59 @@ const Details = () => {
     filterData();
   });
 
+  useEffect(() => {
+    handleBlockScroll(true);
+  }, []);
+
+  useEffect(() => {
+    const page = Math.ceil(Number(id) / itemsPerPage);
+
+    dispatch(setCurrentPage(page));
+  }, [id, dispatch]);
+
   const name = `${detailed?.name.first} ${detailed?.name.last}` || "";
   const url = window.location.href;
-  const gender = detailed?.gender === "male" ? "Masculino" : "Feminino";
+  const gender = detailed?.gender === "male" ? "Male" : "Female";
 
   return (
-    <div className="absolute top-0 flex items-center justify-center h-screen w-screen bg-opacity-40 bg-gray-700 blur-lg">
+    <div className="fixed left-0 top-0 flex items-center justify-center h-screen w-screen bg-opacity-40 bg-gray-700 blur-lg">
       {detailed !== null ? (
-        <div className="p-8 rounded shadow-md bg-gray-300 mt-20 pt-16 relative opacity-100">
+        <div
+          ref={ref}
+          className="p-8 rounded shadow-md bg-gray-100 mt-20 pt-16 relative opacity-100"
+        >
           <img
             className="rounded-full w-36 -mt-36 mx-auto shadow-sm"
             src={detailed?.picture.large}
             alt={name}
           />
-          <Link className="font-bold absolute right-0 top-0 p-4 py-2" to="/">
-            X
-          </Link>
+          <button
+            className="font-bold absolute right-0 top-0 p-2 text-2xl hover:text-gray-700"
+            onClick={() => handleCloseButton()}
+          >
+            <CloseIcon />
+          </button>
           <p className="py-1 mr-1">
-            <span className="font-bold">Nome:</span> {name}
+            <span className="font-bold">Name:</span> {name}
           </p>
           <p className="py-1 mr-1">
             <span className="font-bold">Email:</span> {detailed?.email}
           </p>
           <p className="py-1 mr-1">
-            <span className="font-bold">Gênero:</span> {gender}
+            <span className="font-bold">Gender:</span> {gender}
           </p>
           <p className="py-1 mr-1">
-            <span className="font-bold">Data de nascimento:</span>{" "}
+            <span className="font-bold">Date of birth:</span>{" "}
             {formatDate(detailed?.dob.date || "")}
           </p>
           <p className="py-1 mr-1">
-            <span className="font-bold">Telefone:</span> {detailed?.phone}
+            <span className="font-bold">Phone number:</span> {detailed?.phone}
           </p>
           <p className="py-1 mr-1">
-            <span className="font-bold">Nacionalidade:</span> {detailed?.nat}
+            <span className="font-bold">Nationality:</span> {detailed?.nat}
           </p>
           <p className="py-1 mr-1">
-            <span className="font-bold">Endereço:</span>{" "}
+            <span className="font-bold">Address:</span>{" "}
             {detailed?.location.street.name}-{detailed?.location.street.number},{" "}
             {detailed?.location.city}, {detailed?.location.state},{" "}
             {detailed?.location.country}
